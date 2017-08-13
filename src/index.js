b0592c9278a471a2996e8a5a3b2ade15600cf138
Weston Hunter <weston.hunter@gmail.com>
2017-08-13 18:10:57 -0500

    Youtube 2 implementation.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import _ from 'lodash';
+
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyAnq1KPNh-tzgqMDc3VtfBbJeSucyBf2sY';
 
-const App = () => {
-    return <div>sample component</div>;
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
+
+        this.videoSearch('fpga development boards');
+    }
+
+    videoSearch(term) {
+        YTSearch({key: API_KEY, term: term}, videos => {
+            console.log(videos);
+            this.setState({
+                videos,
+                selectedVideo: videos[0]
+            });
+        });
+    }
+
+    render() {
+        const videoSearch = _.debounce(term => this.videoSearch(term), 1000);
+
+        return (
+            <div>
+                <SearchBar onSearchTermChange={videoSearch} />
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                    videos={this.state.videos} />
+            </div>
+        );
+    }
 }
 
 const container = document.querySelector('div.container');
