198a1370d632412e361fab41eba5436607ca6857
Weston Hunter <weston.hunter@gmail.com>
2018-08-20 14:20:14 -0500

    Youtube viewer: * Debounce search term input. * Retire YT API key.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -6,7 +7,7 @@ import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
 
-const API_KEY = 'AIzaSyAJTv1qW5wwitE8DOhGy6EeuFcort3YUnE';
+const API_KEY = '';
 // const API_KEY = 'AIzaSyB92eQZIgomG-S5ceeR4LUmd6gZjv5OGnk';
 
 class App extends Component {
@@ -22,13 +23,16 @@ class App extends Component {
 
         this.onVideoSelect = this.onVideoSelect.bind(this);
         this.videoSearch = this.videoSearch.bind(this);
+        this.debouncedVideoSearch = _.debounce(term => this.videoSearch(term), 300);
     }
 
     render() {
         const firstVideo = this.state.videos.length > 0 ? this.state.videos[0] : null;
+        // const debouncedVideoSearch = _.debounce((term) => this.videoSearch(term), 300);
+
         return (
             <div>
-                <SearchBar onSearchTermChange={this.videoSearch} searchTerm={'surfboards'} />
+                <SearchBar onSearchTermChange={this.debouncedVideoSearch} searchTerm={'surfboards'} />
                 <VideoDetail video={this.state.selectedVideo} />
                 <VideoList
                     onVideoSelect={this.onVideoSelect}
