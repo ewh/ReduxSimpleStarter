2475bed68d11cea36cfdef59f6f6cee0aed7e656
Weston Hunter <weston.hunter@gmail.com>
2018-08-24 22:27:38 -0500

    Weather tutorial: start.

---
+++ src/containers/search_bar.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+export default class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {term: ''};
+        this.onInputChange = this.onInputChange.bind(this);
+    }
+
+    onInputChange(event) {
+        console.log('onInputChange', event.target.value);
+        this.setState({term: event.target.value});
+    }
+
+    onFormSubmit(event) {
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <form className="input-group" onSubmit={this.onFormSubmit}>
+                <input
+                    className="form-control"
+                    placeholder="Fetch 5-day forecast"
+                    value={this.state.term}
+                    onChange={this.onInputChange}
+                />
+                <span className="input-group-btn">
+                    <button type="submit" className="btn btn-secondary">Submit</button>
+                </span>
+            </form>
+        );
+    }
+}
