f8b6d4b46230e95ed476e94cbc553bab5293b270
Weston Hunter <weston.hunter@gmail.com>
2018-09-23 14:12:21 -0400

    Blog Post tutorial: initial implementation.

---
+++ src/components/posts_index.js
@@ -0,0 +1,47 @@
+import _ from 'lodash';
+import React, {Component, Fragment} from 'react';
+import {bindActionCreators} from 'redux';
+import {connect} from 'react-redux';
+import {fetchPosts} from '../actions';
+
+class PostsIndex extends Component {
+    componentDidMount() {
+        this.props.fetchPosts();
+    }
+
+    renderPosts() {
+        // console.log('posts', this.props.posts);
+
+        return _.map(this.props.posts, post => {
+            return (
+                <li className="list-group-item" key={post.id}>
+                    {post.title}
+                </li>
+            );
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h3>Posts</h3>
+                <ul className="list-group">
+                    {this.renderPosts()}
+                </ul>
+            </div>
+        );
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({fetchPosts}, dispatch);
+}
+
+function mapStateToProps({posts}) {
+    console.log('mapStateToProps', posts);
+    return {
+        posts
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(PostsIndex);
